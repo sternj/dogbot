41249d170697d9926ef5b4772223eda27c4b4727
Sam Stern <sam@samstern.me>
2020-03-25 20:28:29 -0400

    added download

--- flaskapp.py
+++ flaskapp.py
@@ -1,5 +1,6 @@
 from flask import Flask, send_file, render_template
 import io
+import nltk
 from dogs.captioner import caption
 app = Flask(__name__)
 
@@ -14,3 +15,7 @@ def send_dog():
     img.save(img_recv, "JPEG")
     img_recv.seek(0)
     return send_file(img_recv, mimetype='image/jpeg', as_attachment=False)
+
+@app.before_first_request
+def download_punkt():
+    nltk.download('punkt')
