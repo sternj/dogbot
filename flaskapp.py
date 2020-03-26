fd10e28c33802525614cd4e1195a6fb094aafa59
Sam Stern <sam@samstern.me>
2020-03-25 20:25:55 -0400

    added frontend

--- flaskapp.py
+++ flaskapp.py
@@ -1,6 +1,16 @@
-from flask import Flask
+from flask import Flask, send_file, render_template
+import io
+from dogs.captioner import caption
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return render_template('index.html')
+
+@app.route('/dog')
+def send_dog():
+    img = caption.put_text(True)
+    img_recv = io.BytesIO()
+    img.save(img_recv, "JPEG")
+    img_recv.seek(0)
+    return send_file(img_recv, mimetype='image/jpeg', as_attachment=False)
